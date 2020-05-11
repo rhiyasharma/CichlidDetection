499a1995002cad1692ecf05ca70f68e658d38892
Tucker L <tuckerlancaster@gmail.com>
2020-05-11 13:10:30 -0400

    starting to flesh out repo structure

---
+++ CichlidDetection/executeTraining.py
@@ -0,0 +1,6 @@
+# picks up where prepareTrainingData leaves off and trains the chosen network
+
+# takes in a csv file of good annotations, the path to a folder of images, and the name of the network to train, and
+# outputs the training and validation for the chosen network
+
+
