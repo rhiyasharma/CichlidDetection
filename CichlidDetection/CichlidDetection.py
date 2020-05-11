bfdb30051030471db5b44551a6ee098326992e4f
Tucker L <tuckerlancaster@gmail.com>
2020-05-11 15:46:00 -0400

    converting prepareTrainingData from a command line executable to an importable function for integration into CichlidDetection.py

--- CichlidDetection/CichlidDetection.py
+++ CichlidDetection/CichlidDetection.py
@@ -1,5 +1,5 @@
 import argparse
+from CichlidDetection.PrepareTrainingData import prep_data
 
 # main script, meant to be run from the command line.
-
 
--- CichlidDetection/PrepareTrainingData.py
+++ CichlidDetection/PrepareTrainingData.py
@@ -10,6 +10,7 @@ import numpy as np
 import matplotlib.image as mpimg
 import cv2
 
+
 # 'command+Q' to shut the window; left arrow to display all modified image frames windows
 # Run the script using the command: python3 prepareTrainingData.py <ProjectID>
 # ex: python3 prepareTrainingData.py MC6_5
@@ -17,135 +18,136 @@ import cv2
 # Add '-v' or '--view' flag to view modified dataset
 
 
-def initialising():
-	rcloneRemote = 'cichlidVideo'
-	output = subprocess.run(['rclone', 'lsf', rcloneRemote + ':'], capture_output = True, encoding = 'utf-8')
-	if 'McGrath/' in output.stdout.split():
-		cloudMasterDir = rcloneRemote + ':McGrath/Apps/CichlidPiData/'
-	elif 'BioSci-McGrath/' in output.stdout.split():
-		cloudMasterDir = rcloneRemote + ':BioSci-McGrath/Apps/CichlidPiData/'
-	else:
-		raise Exception('Cant find master McGrath directory in rclone remote')
+def initialising(pid):
+    rcloneRemote = 'cichlidVideo'
+    output = subprocess.run(['rclone', 'lsf', rcloneRemote + ':'], capture_output=True, encoding='utf-8')
+    if 'McGrath/' in output.stdout.split():
+        cloudMasterDir = rcloneRemote + ':McGrath/Apps/CichlidPiData/'
+    elif 'BioSci-McGrath/' in output.stdout.split():
+        cloudMasterDir = rcloneRemote + ':BioSci-McGrath/Apps/CichlidPiData/'
+    else:
+        raise Exception('Cant find master McGrath directory in rclone remote')
 
-	annotated_data_path = '__AnnotatedData/BoxedFish/BoxedFish.csv'
+    annotated_data_path = '__AnnotatedData/BoxedFish/BoxedFish.csv'
 
-	image_data_path = '__AnnotatedData/BoxedFish/BoxedImages/{}'.format(imgfile_name) # Add .format{}; include argument to accept project ID
+    image_data_path = '__AnnotatedData/BoxedFish/BoxedImages/{}.tar'.format(pid)
+    remoteDir = cloudMasterDir + '{}'.format(pid)
+    remote_files = subprocess.run(['rclone', 'lsf', remoteDir], capture_output=True, encoding='utf-8')
 
-	remoteDir = cloudMasterDir + '{}'.format(args.ProjectID)
-	remote_files = subprocess.run(['rclone', 'lsf', remoteDir], capture_output = True, encoding = 'utf-8')
+    if 'videoCropPoints.npy' and 'videoCrop.npy' in remote_files.stdout.split():
+        video_points_data_path = '{}/videoCropPoints.npy'.format(pid)
+        video_crop_data_path = '{}/videoCrop.npy'.format(pid)
+    else:
+        video_points_data_path = '{}/MasterAnalysisFiles/VideoPoints.npy'.format(pid)
+        video_crop_data_path = '{}/MasterAnalysisFiles/VideoCrop.npy'.format(pid)
 
-	if 'videoCropPoints.npy' and 'videoCrop.npy' in remote_files.stdout.split():
-		video_points_data_path = '{}/videoCropPoints.npy'.format(args.ProjectID)
-		video_crop_data_path = '{}/videoCrop.npy'.format(args.ProjectID)
-	else:
-		video_points_data_path = '{}/MasterAnalysisFiles/VideoPoints.npy'.format(args.ProjectID)
-		video_crop_data_path = '{}/MasterAnalysisFiles/VideoCrop.npy'.format(args.ProjectID)
+    localDir = os.getenv('HOME') + '/' + 'Desktop/McGrathLab/'
+    if not os.path.exists(localDir):
+        os.mkdir(localDir)
 
-	localDir = os.getenv('HOME') + '/' + 'Desktop/McGrathLab/'
-	if not os.path.exists(localDir):
-		os.mkdir(localDir)
+    return cloudMasterDir, annotated_data_path, image_data_path, video_points_data_path, video_crop_data_path, localDir
 
-	return cloudMasterDir, annotated_data_path, image_data_path, video_points_data_path, video_crop_data_path, localDir
 
 # Download files from cloud directory to local directory and read it in
-def download(cloudMasterDir, annotated_data, image_data, video_points_file, video_crop_file, localDir, imgfile_name):
+def download(pid, cloudMasterDir, annotated_data, image_data, video_points_file, video_crop_file, localDir, imgfile_name):
+    # Creating a subdirectory
+    subdir_name = image_data.split('/')[3].split('.')[0]
+    subdir = os.path.join(localDir, subdir_name)
 
-	# Creating a subdirectory
-	subdir_name = image_data.split('/')[3].split('.')[0]
-	subdir = os.path.join(localDir, subdir_name)
+    # Check if subdir is already there, and create it if it is not
+    if not os.path.exists(subdir):
+        os.mkdir(subdir)
 
-	# Check if subdir is already there, and create it if it is not
-	if not os.path.exists(subdir):
-		os.mkdir(subdir)
+    remoteDir = cloudMasterDir + '{}'.format(pid)
+    remote_files = subprocess.run(['rclone', 'lsf', remoteDir], capture_output=True, encoding='utf-8')
 
-	remoteDir = cloudMasterDir + '{}'.format(args.ProjectID)
-	remote_files = subprocess.run(['rclone', 'lsf', remoteDir], capture_output = True, encoding = 'utf-8')
+    os.chdir(subdir)
+    localDir = os.getcwd()
 
+    files = os.listdir()
 
-	os.chdir(subdir)
-	localDir = os.getcwd()
+    file1 = 'BoxedFish.csv'
+    file2 = imgfile_name
+    if 'videoCropPoints.npy' in remote_files.stdout.split():
+        file3 = 'videoCropPoints.npy'
+    else:
+        file3 = 'VideoPoints.npy'
+    file4 = 'VideoCrop.npy'
 
-	files = os.listdir()
+    if file1 not in files:
+        print('Downloading annotations')
+        # Download annotations
+        downloaded_file_1 = subprocess.run(['rclone', 'copy', cloudMasterDir + annotated_data, localDir],
+                                           stderr=subprocess.PIPE)
 
-	file1 = 'BoxedFish.csv'
-	file2 = imgfile_name
-	if 'videoCropPoints.npy' in remote_files.stdout.split():
-		file3 = 'videoCropPoints.npy'
-	else:
-		file3 = 'VideoPoints.npy'
-	file4 = 'VideoCrop.npy'
+    if file2 not in files:
+        print('Downloading images')
+        # Download images
+        downloaded_file_2 = subprocess.run(['rclone', 'copy', cloudMasterDir + image_data, localDir],
+                                           stderr=subprocess.PIPE)
 
+    if file3 not in files:
+        print('Downloading video points')
+        # Download video points file
+        downloaded_file_3 = subprocess.run(['rclone', 'copy', cloudMasterDir + video_points_file, localDir],
+                                           stderr=subprocess.PIPE)
 
-	if file1 not in files:
-		print('Downloading annotations')
-		# Download annotations
-		downloaded_file_1 = subprocess.run(['rclone', 'copy', cloudMasterDir + annotated_data, localDir], stderr = subprocess.PIPE)
+    if file4 not in files:
+        print('Downloading video crop')
+        # Download video points file
+        downloaded_file_4 = subprocess.run(['rclone', 'copy', cloudMasterDir + video_crop_file, localDir],
+                                           stderr=subprocess.PIPE)
 
-	if file2 not in files:
-		print('Downloading images')
-		# Download images
-		downloaded_file_2 = subprocess.run(['rclone', 'copy', cloudMasterDir + image_data, localDir], stderr = subprocess.PIPE)
+    return localDir, file1, file2, file3, file4
 
-	if file3 not in files:
-		print('Downloading video points')
-		# Download video points file
-		downloaded_file_3 = subprocess.run(['rclone', 'copy', cloudMasterDir + video_points_file, localDir], stderr = subprocess.PIPE)
-
-	if file4 not in files:
-		print('Downloading video crop')
-		# Download video points file
-		downloaded_file_4 = subprocess.run(['rclone', 'copy', cloudMasterDir + video_crop_file, localDir], stderr = subprocess.PIPE)
-
-
-	return localDir, file1, file2, file3, file4
 
 # --------------------------------- Area Calculation ---------------------------------#
 
 def area(box):
+    if 'VideoPoints.npy' in os.listdir():
+        vpfile = 'VideoPoints.npy'
+    elif 'videoCropPoints.npy' in os.listdir():
+        vpfile = 'videoCropPoints.npy'
 
-	if 'VideoPoints.npy' in os.listdir():
-		vpfile = 'VideoPoints.npy'
-	elif 'videoCropPoints.npy' in os.listdir():
-		vpfile = 'videoCropPoints.npy'
+    # video points array:
+    vp_array = np.load(vpfile)
 
-	# video points array:
-	vp_array = np.load(vpfile)
+    # Video crop coordinates
+    v0 = list(vp_array[0])
+    # v0_x, v0_y = v0
+    v1 = list(vp_array[1])
+    # v1_x, v0_y = v1
+    v2 = list(vp_array[2])
+    # v2_x, v0_y = v0
+    v3 = list(vp_array[3])
+    # v3_x, v0_y = v0
 
-	# Video crop coordinates
-	v0 = list(vp_array[0])
-	# v0_x, v0_y = v0
-	v1 = list(vp_array[1])
-	# v1_x, v0_y = v1
-	v2 = list(vp_array[2])
-	# v2_x, v0_y = v0
-	v3 = list(vp_array[3])
-	# v3_x, v0_y = v0
+    # Create polygon for the video points
+    poly_vp = Polygon([v0, v1, v2, v3])
 
+    # Annotation box coordinates
+    x_a, y_a, w_a, h_a = box
 
-	# Create polygon for the video points 
-	poly_vp = Polygon([v0, v1, v2, v3])
+    # Create polygon for the annotation box
+    poly_ann = Polygon([[x_a, y_a], [x_a + w_a, y_a], [x_a + w_a, y_a + h_a], [x_a, y_a + h_a]])
 
-	# Annotation box coordinates
-	x_a, y_a, w_a, h_a = box
+    intersection_area = poly_ann.intersection(poly_vp).area
+    ann_area = poly_ann.area
 
-	# Create polygon for the annotation box 
-	poly_ann = Polygon([[x_a,y_a],[x_a+w_a,y_a],[x_a+w_a,y_a+h_a],[x_a,y_a+h_a]])
+    return (intersection_area, ann_area)
 
-	intersection_area = poly_ann.intersection(poly_vp).area 
-	ann_area = poly_ann.area
-
-	return (intersection_area, ann_area)
 
 # --------------------------------- Determining Whether Annotations Lie Within Crop ---------------------------------#
 
 def determine(area):
-	intersection_area, ann_area = area
-	if intersection_area == ann_area:
-		verdict = "Yes"
-	else:
-		verdict = "No"
+    intersection_area, ann_area = area
+    if intersection_area == ann_area:
+        verdict = "Yes"
+    else:
+        verdict = "No"
 
-	return verdict
+    return verdict
+
 
 # --------------------------------- Plot Images ---------------------------------#
 '''
@@ -191,60 +193,54 @@ def plotImage(data):
 
 
 def prep_data(pid, view=False):
-	# --------------------------------- Downloading The Data ---------------------------------#
-	imgfile_name = '{}.tar'.format(pid)
-	cloud_master_directory, annotated_data_path, image_file_path, video_points_file_path, video_crop_file_path, local_directory = initialising()
-	localDir, ann_file, img_file, vp_file, vc_file = download(cloud_master_directory, annotated_data_path, image_file_path, video_points_file_path, video_crop_file_path, local_directory, imgfile_name)
-	print("Download complete for {}!".format(pid))
+    # --------------------------------- Downloading The Data ---------------------------------#
+    imgfile_name = '{}.tar'.format(pid)
+    cloud_master_directory, annotated_data_path, image_file_path, video_points_file_path, video_crop_file_path, local_directory = initialising(pid)
+    localDir, ann_file, img_file, vp_file, vc_file = download(pid, cloud_master_directory, annotated_data_path,
+                                                              image_file_path, video_points_file_path,
+                                                              video_crop_file_path, local_directory, imgfile_name)
+    print("Download complete for {}!".format(pid))
 
-	# --------------------------------- Iterating Through Annotations ---------------------------------#
+    # --------------------------------- Iterating Through Annotations ---------------------------------#
 
-	# video points array:
-	vp_array = np.load(vp_file)
+    # video points array:
+    vp_array = np.load(vp_file)
 
-	# Video crop coordinates
-	x_vp = tuple(vp_array[0])
-	y_vp = tuple(vp_array[1])
-	w_vp = tuple(vp_array[2])
-	h_vp = tuple(vp_array[3])
+    # Video crop coordinates
+    x_vp = tuple(vp_array[0])
+    y_vp = tuple(vp_array[1])
+    w_vp = tuple(vp_array[2])
+    h_vp = tuple(vp_array[3])
 
-	# Read in BoxFile.csv file
-	df = pd.read_csv(localDir + '/' + ann_file)
-	df = df[df['ProjectID'] == pid]
-	df = df[df['CorrectAnnotation'] == 'Yes']
-	df = df.fillna('')
-	df = df[df['Box'] != '']
-	df['Box']=[eval(i) for i in df['Box']]
-	pd.set_option('display.max_rows', None)
-	df['Area'] = df['Box'].apply(area)
+    # Read in BoxFile.csv file
+    df = pd.read_csv(localDir + '/' + ann_file)
+    df = df[df['ProjectID'] == pid]
+    df = df[df['CorrectAnnotation'] == 'Yes']
+    df = df.fillna('')
+    df = df[df['Box'] != '']
+    df['Box'] = [eval(i) for i in df['Box']]
+    pd.set_option('display.max_rows', None)
+    df['Area'] = df['Box'].apply(area)
 
-	#  Iterate through the annotations and identify annotations that fall within the video crop
-	df['WithinCrop'] = df['Area'].apply(determine)
-	df = df[df['WithinCrop'] == 'Yes']
-	# print(df)
-	export_CorrectAnn_csv = df.to_csv(r'CorrectAnnotations.csv', index = None, header=True)
-	framefiles = list(df.groupby('Framefile').count().index)
-	print('Number of annotated frames that lie within the video crop: ', len(framefiles))
-	if view:
-		for frame in framefiles:
-			#df_row = df[df['Framefile'] == frame]
-			#df_row[['Framefile', 'Box']].apply(plotImage, axis=1)
-			img_dir = img_file.split('.')[0]
-			if img_dir not in os.listdir():
-				tf = tarfile.open(img_file)
-				tf.extractall()
-			image_frame = img_dir+'/'+frame
-			img = cv2.imread(image_frame)
-			mask = np.load(vc_file)
-			mask = np.logical_not(mask)
-			img[mask] = 0
-			cv2.imshow("Modified Frame: " + frame, img)
-			cv2.waitKey(0)
-
-
-
-
-
-
-
-
+    #  Iterate through the annotations and identify annotations that fall within the video crop
+    df['WithinCrop'] = df['Area'].apply(determine)
+    df = df[df['WithinCrop'] == 'Yes']
+    # print(df)
+    export_CorrectAnn_csv = df.to_csv(r'CorrectAnnotations.csv', index=None, header=True)
+    framefiles = list(df.groupby('Framefile').count().index)
+    print('Number of annotated frames that lie within the video crop: ', len(framefiles))
+    if view:
+        for frame in framefiles:
+            # df_row = df[df['Framefile'] == frame]
+            # df_row[['Framefile', 'Box']].apply(plotImage, axis=1)
+            img_dir = img_file.split('.')[0]
+            if img_dir not in os.listdir():
+                tf = tarfile.open(img_file)
+                tf.extractall()
+            image_frame = img_dir + '/' + frame
+            img = cv2.imread(image_frame)
+            mask = np.load(vc_file)
+            mask = np.logical_not(mask)
+            img[mask] = 0
+            cv2.imshow("Modified Frame: " + frame, img)
+            cv2.waitKey(0)
