496cd400d0be5154d964ba2525117a2485dfff22
Tucker L <tuckerlancaster@gmail.com>
2020-05-12 17:28:37 -0400

    trying to fix module not found error when running from command line

--- CichlidDetection/runner.py
+++ CichlidDetection/runner.py
@@ -1,7 +1,8 @@
-import argparse
+import argparse, os, sys
 from CichlidDetection.Classes.DataPrepper import DataPrepper
 
-# main script, meant to be run from the command line.
+# main script. Can be run from the command line by navigating outermost CichlidDetection folder and
+# calling 'python -m CichlidDetection.runner'
 
 dataprepper = DataPrepper('MC6_5')
 dataprepper.prep()
