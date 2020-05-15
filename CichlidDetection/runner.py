c8fa5ef16c64b3459f55496cbf59577e5df23be3
Lijiang Long <llong35@gatech.edu>
2020-05-15 11:12:29 -0400

    add pdb to runner

--- CichlidDetection/runner.py
+++ CichlidDetection/runner.py
@@ -1,8 +1,9 @@
 import argparse, os, sys
 from CichlidDetection.Classes.DataPrepper import DataPrepper
+import pdb
 
 # main script. Can be run from the command line by navigating outermost CichlidDetection folder and
 # calling 'python -m CichlidDetection.runner'
-
+pdb.set_trace()
 dataprepper = DataPrepper('MC6_5')
 dataprepper.YOLO_prep()
