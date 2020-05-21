b760eef992b3e119e8851a4b44d0d52fbfddc926
Lijiang Long <llong35@gatech.edu>
2020-05-21 10:58:20 -0400

    test

--- CichlidDetection/Classes/DataLoader/FasterRCNN_loader.py
+++ CichlidDetection/Classes/DataLoader/FasterRCNN_loader.py
@@ -1,60 +1,73 @@
+import os
+import pandas as pd
+from collections import defaultdict
+from PIL import Image
+import torch
+
 class CicilidDataset(object):
-    def __init__(self, root, transforms):
+    def __init__(self, root, transforms,subset):
         self.root = root
         self.transforms = transforms
-        # load all image files, sorting them to
-        # ensure that they are aligned
-        self.imgs = list(sorted(os.listdir(os.path.join(root, "PNGImages"))))
-        self.masks = list(sorted(os.listdir(os.path.join(root, "PedMasks"))))
+        self.boxes = defaultdict(list)
+        self.labels = defaultdict(list)
+        
+        
+        boxed_file = os.path.join(root,'None','BoxedFish.csv')
+        if subset == 'training':
+            self.files_list = os.path.join(root,'train_list.txt')
+        elif subset == 'test':
+            self.files_list = os.path.join(root,'test_list.txt')
+        
+        df = pd.read_csv(boxed_file)
+        for index, row in df.iterrows():
+            if row.Nfish == 0 or not row.Sex or row.Sex == 'u' or row.CorrectAnnotation == 'No':
+                continue
+            key = row.ProjectID+'/images/'+row.Framefile
+            self.boxes[key].append(row.Box)
+            if row.Sex == 'm':
+                self.labels[key].append(0)
+            else:
+                self.labels[key].append(1)
+        self.imgs = []
+        with open(self.files_list,'r') as input:
+            for line in input:
+                self.imgs.append(line.rstrip())
+
 
     def __getitem__(self, idx):
         # load images ad masks
-        img_path = os.path.join(self.root, "PNGImages", self.imgs[idx])
-        mask_path = os.path.join(self.root, "PedMasks", self.masks[idx])
+        key = self.imgs[idx]
+        img_path = os.path.join(self.root, key)
         img = Image.open(img_path).convert("RGB")
-        # note that we haven't converted the mask to RGB,
-        # because each color corresponds to a different instance
-        # with 0 being background
-        mask = Image.open(mask_path)
-
-        mask = np.array(mask)
-        # instances are encoded as different colors
-        obj_ids = np.unique(mask)
-        # first id is the background, so remove it
-        obj_ids = obj_ids[1:]
-
-        # split the color-encoded mask into a set
-        # of binary masks
-        masks = mask == obj_ids[:, None, None]
-
-        # get bounding box coordinates for each mask
-        num_objs = len(obj_ids)
+        box_old = self.boxes[key]
         boxes = []
-        for i in range(num_objs):
-            pos = np.where(masks[i])
-            xmin = np.min(pos[1])
-            xmax = np.max(pos[1])
-            ymin = np.min(pos[0])
-            ymax = np.max(pos[0])
-            boxes.append([xmin, ymin, xmax, ymax])
 
+        for i,box in enumerate(box_old):
+            box = [int(x) for x in box[1:-1].split(',')]
+            xmin = box[0]
+            xmax = box[0] + box[2]
+            ymin = box[1]
+            ymax = box[1] + box[3]
+            boxes.append([xmin, ymin, xmax, ymax])
+        labels = self.labels[key]
+        assert len(labels) == len(boxes)
         boxes = torch.as_tensor(boxes, dtype=torch.float32)
-        # there is only one class
-        labels = torch.ones((num_objs,), dtype=torch.int64)
-        masks = torch.as_tensor(masks, dtype=torch.uint8)
+       
+        labels = torch.as_tensor(labels, dtype=torch.int64)
+
 
         image_id = torch.tensor([idx])
         area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
         # suppose all instances are not crowd
-        iscrowd = torch.zeros((num_objs,), dtype=torch.int64)
+#         iscrowd = torch.zeros((num_objs,), dtype=torch.int64)
 
         target = {}
         target["boxes"] = boxes
         target["labels"] = labels
-        target["masks"] = masks
+#         target["masks"] = masks
         target["image_id"] = image_id
         target["area"] = area
-        target["iscrowd"] = iscrowd
+#         target["iscrowd"] = iscrowd
 
         if self.transforms is not None:
             img, target = self.transforms(img, target)
