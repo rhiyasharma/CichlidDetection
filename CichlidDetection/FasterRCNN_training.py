33287d182b0348920e05fb5709e46070ea97e617
Lijiang Long <llong35@gatech.edu>
2020-05-21 15:47:41 -0400

    test

--- CichlidDetection/FasterRCNN_training.py
+++ CichlidDetection/FasterRCNN_training.py
@@ -5,7 +5,7 @@ from PIL import Image
 
 import pdb
 
-from Utilities.utils import Logger,AverageMeter
+from Utilities.utils import Logger,AverageMeter,collate_fn
 import Utilities.transforms as T
 
 from Classes.DataLoader.FasterRCNN_loader import CicilidDataset
@@ -118,10 +118,10 @@ def main():
 
 # define training and validation data loaders
     train_loader = torch.utils.data.DataLoader(
-        train_dataset, batch_size=10, shuffle=True, num_workers=2,pin_memory=True,collate_fn=utils.collate_fn)
+        train_dataset, batch_size=10, shuffle=True, num_workers=2,pin_memory=True,collate_fn=collate_fn)
 
     test_loader = torch.utils.data.DataLoader(
-        test_dataset, batch_size=10, shuffle=False, num_workers=2,pin_memory=True,collate_fn=utils.collate_fn)
+        test_dataset, batch_size=10, shuffle=False, num_workers=2,pin_memory=True,collate_fn=collate_fn)
 
     optimizer = optim.SGD(parameters, lr=0.005,
                                 momentum=0.9, weight_decay=0.0005)
