b2f30e8f42728c1949cfad18906f69d04c724ad3
Tucker L <tuckerlancaster@gmail.com>
2020-05-28 17:42:25 -0400

    debugging

--- CichlidDetection/Runner.py
+++ CichlidDetection/Runner.py
@@ -9,6 +9,7 @@ from CichlidDetection.Utilities.SystemUtilities import run
 class Runner:
     def __init__(self):
         self.fm = FileManager()
+        self.dp = DataPrepper(self.fm)
         self.__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
         pass
 
