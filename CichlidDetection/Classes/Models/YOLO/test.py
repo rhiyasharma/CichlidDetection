0b0107bbc507ec33ea513b5e9545444173a8cac0
Tucker L <tuckerlancaster@gmail.com>
2020-06-03 15:01:41 -0400

    upping verbosity

--- CichlidDetection/Classes/Models/YOLO/test.py
+++ CichlidDetection/Classes/Models/YOLO/test.py
@@ -47,7 +47,7 @@ def test(cfg,
             model = nn.DataParallel(model)
     else:  # called by train.py
         device = next(model.parameters()).device  # get model device
-        verbose = False
+        verbose = True
 
     # Configure run
     data = parse_data_cfg(data)
