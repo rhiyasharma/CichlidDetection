c452545302b3708a9b52617f3b69d31ad5a7c3a3
Tucker L <tuckerlancaster@gmail.com>
2020-06-05 13:23:32 -0400

    clarifying which Model is used

--- CichlidDetection/Runner.py
+++ CichlidDetection/Runner.py
@@ -13,7 +13,7 @@ class Runner:
         self.__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
         pass
 
-    def prep(self, pids='All'):
+    def prep_yolo(self, pids='All'):
         if pids is 'All':
             pids = self.fm.get_all_pids()
         for pid in pids:
@@ -21,7 +21,7 @@ class Runner:
             self.dp = DataPrepper(self.fm)
             self.dp.YOLO_prep()
 
-    def train(self):
+    def train_yolo(self):
         model_dir = os.path.join(self.__location__, 'Classes', 'Models', 'YOLO')
         pbs_file = os.path.join(model_dir, 'train.pbs')
         data_file = self.fm.local_files['data_file']
