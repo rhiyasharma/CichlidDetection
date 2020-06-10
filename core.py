3de5a1cfd6a9e65be3ea024e1e3f48848e32b77a
Tucker L <tuckerlancaster@gmail.com>
2020-06-10 12:15:20 -0400

    modifying slightly to clarify that running in PBS mode automatically includes the download phase

--- core.py
+++ core.py
@@ -5,11 +5,19 @@ import argparse, subprocess, os
 
 parser = argparse.ArgumentParser()
 subparsers = parser.add_subparsers(help='Available Commands', dest='command')
-download_parser = subparsers.add_parser('download')
-train_parser = subparsers.add_parser('train')
 
+download_parser = subparsers.add_parser('download')
+
+train_parser = subparsers.add_parser('train')
 train_parser.add_argument('-e', '--Epochs', type=int, default=10)
-train_parser.add_argument('--pbs', action='store_true')
+
+full_auto_parser = subparsers.add_parser('full_auto')
+full_auto_parser.add_argument('-e', '--Epochs', type=int, default=10)
+full_auto_parser.add_argument('--pbs', action='store_true')
+
+
+
+
 args = parser.parse_args()
 
 runner = Runner()
@@ -19,11 +27,14 @@ if args.command == 'download':
     runner.download()
 
 elif args.command == 'train':
+    runner.prep()
+    runner.train(num_epochs=args.Epochs)
+
+elif args.command == 'full_auto':
     if args.pbs:
         pbs_dir = os.path.join(package_root, 'CichlidDetection/PBS')
-        subprocess.run(['cd {}; qsub train.pbs'.format(pbs_dir)])
+        subprocess.run(['cd {}; qsub train.pbs -v EPOCHS="{}"'.format(pbs_dir, args.Epochs)])
     else:
+        runner.download()
         runner.prep()
         runner.train(num_epochs=args.Epochs)
-
-
