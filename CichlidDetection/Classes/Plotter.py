11cef9521883683e9e7a6bf0c2a61e0aa5d3f6cc
Tucker L <tuckerlancaster@gmail.com>
2020-06-12 13:38:56 -0400

    outlining a Plotter class

---
+++ CichlidDetection/Classes/Plotter.py
@@ -0,0 +1,26 @@
+from CichlidDetection.Classes.FileManagers import FileManager
+
+
+class Plotter:
+
+    def __init__(self):
+        self.fm = FileManager
+        self._load_data()
+
+    def plot_all(self):
+        pass
+
+    def plot_iou(self):
+        pass
+
+    def plot_precision(self):
+        pass
+
+    def plot_recall(self):
+        pass
+
+    def plot_loss(self):
+        pass
+
+    def _load_data(self):
+        pass
