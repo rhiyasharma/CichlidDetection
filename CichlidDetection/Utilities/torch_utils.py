a95780b9e316e1fbaad7bbc150d9300094426d14
Tucker L <tuckerlancaster@gmail.com>
2020-06-19 12:36:31 -0400

    diverting metric logger output to file

--- CichlidDetection/Utilities/torch_utils.py
+++ CichlidDetection/Utilities/torch_utils.py
@@ -186,39 +186,18 @@ class MetricLogger(object):
         if not header:
             header = ''
         space_fmt = ':' + str(len(str(len(iterable)))) + 'd'
-        if torch.cuda.is_available():
-            log_msg = self.delimiter.join([
-                header,
-                '[{0' + space_fmt + '}/{1}]',
-                'eta: {eta}',
-                '{meters}',
-                'time: {time}',
-                'data: {data}',
-                'max mem: {memory:.0f}'
+        log_msg = self.delimiter.join([
+            header,
+            '[{0' + space_fmt + '}/{1}]',
+            '{meters}',
             ])
-        else:
-            log_msg = self.delimiter.join([
-                header,
-                '[{0' + space_fmt + '}/{1}]',
-                'eta: {eta}',
-                '{meters}',
-                'time: {time}',
-                'data: {data}'
-            ])
-        MB = 1024.0 * 1024.0
         for obj in iterable:
             yield obj
             if i % print_freq == 0 or i == len(iterable) - 1:
-                if torch.cuda.is_available():
-                    print(log_msg.format(
-                        i, len(iterable),
-                        meters=str(self)),
-                        file=self.f)
-                else:
-                    print(log_msg.format(
-                        i, len(iterable),
-                        meters=str(self)),
-                        file=self.f)
+                print(log_msg.format(
+                    i, len(iterable),
+                    meters=str(self)),
+                    file=self.f)
             i += 1
 
 
