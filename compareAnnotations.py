6a10c5553ace03d8b1cbb8abe4d48a256546880a
rsharma348 <rsharma348@gatech.edu>
2020-06-26 19:21:55 -0400

    Added plots

--- compareAnnotations.py
+++ compareAnnotations.py
@@ -1,6 +1,7 @@
 import pdb, matplotlib, os, argparse
 import numpy as np
 import pandas as pd
+pd.options.mode.chained_assignment = None
 import seaborn as sns
 import matplotlib.pyplot as plt
 from os.path import join, exists
@@ -22,6 +23,8 @@ class CompareAnnotations:
 						 'labels_predicted': 'Sex_ml', 'average_iou': 'IOU'}, inplace=True)
 
 		sex_agree = []
+		gender_actual = []
+		gender_pred=[]
 
 		for row in dt.itertuples():
 			ann1 = row.Box_man
@@ -29,6 +32,8 @@ class CompareAnnotations:
 			sex1 = row.Sex_man
 			sex2 = row.Sex_ml
 			iou = row.IOU
+			actual_sex = eval(row.Sex_man)
+			pred_sex = eval(row.Sex_ml)
 
 			try:
 				ann1 = eval(ann1)
@@ -45,9 +50,22 @@ class CompareAnnotations:
 			else:
 				sex_agree.append('No fish')
 
-		dt['SexAgree'] = pd.Series(sex_agree)
 
-		dt = dt[['Framefile', 'Box_man', 'Sex_man', 'Box_ml', 'Sex_ml', 'SexAgree', 'IOU']]
+			if len(set(pred_sex)) == 0:
+				gender_pred.append(0)
+			elif len(set(pred_sex)) == 1:
+				if pred_sex[0] == 1:
+					gender_pred.append(1)
+				else:
+					gender_pred.append(2)
+			elif len(set(pred_sex)) == 2:
+				gender_pred.append(3)
+
+		dt['SexAgree'] = pd.Series(sex_agree)
+		# dt['Gender_Actual'] = pd.Series(gender_actual)
+		dt['Gender_Predicted'] = pd.Series(gender_pred)
+
+		dt = dt[['Framefile', 'Box_man', 'Sex_man', 'Box_ml', 'Sex_ml', 'Gender_Predicted', 'SexAgree', 'IOU', 'avg_accuracy']]
 		return dt
 
 	def plotPhoto(frame, dt2, img_dir):
@@ -90,20 +108,79 @@ class CompareAnnotations:
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-pl', '--plot', action = 'store_true', help = 'Use this flag to view the disagreements')
+parser.add_argument('-v', '--view', action = 'store_true', help = 'Use this flag to view the disagreements')
 parser.add_argument('-s', '--save', action='store_true', help='Use this flag to save dataframe as csv')
+parser.add_argument('-p', '--plot', action='store_true', help='Use this flag to plot male/female analysis')
 args = parser.parse_args()
 
 comparer = CompareAnnotations()
 dt1 = pd.read_csv(comparer.data)
 dt = CompareAnnotations.compare(dt1)
+dt_f = dt[dt['Gender_Predicted'] == 1]
+dt_m = dt[dt['Gender_Predicted'] == 2]
+dt_b = dt[dt['Gender_Predicted'] == 3]
+dt_n = dt[dt['Gender_Predicted'] == 0] # No fish; 82 frames with no fish
+
+dt_f = dt_f.groupby(dt_f['avg_accuracy']).count().reset_index() # females
+dt_m = dt_m.groupby(dt_m['avg_accuracy']).count().reset_index() # males
+dt_b = dt_b.groupby(dt_b['avg_accuracy']).count().reset_index() # both
+
 
 if args.save:
 	dt.to_csv(join(comparer.csv_dir, 'CompareAnnotations.csv'))
 
-dt_disagreements = dt[dt.SexAgree=='False']
-
-framefiles = dt_disagreements.groupby('Framefile').count().index
-if args.plot:
+if args.view:
+	dt_disagreements = dt[dt.SexAgree == 'False']
+	framefiles = dt_disagreements.groupby('Framefile').count().index
 	for frame in framefiles:
 		CompareAnnotations.plotPhoto(frame, dt_disagreements, comparer.img_dir)
+
+
+
+##### Plotting #####
+fig = plt.figure()
+# fig.set_size_inches(11, 8.5)
+# fig.tight_layout()
+ax1 = fig.add_subplot(221)   #top left
+ax2 = fig.add_subplot(222)   #top right
+ax3 = fig.add_subplot(223)   #bottom left
+ax4 = fig.add_subplot(224)   #bottom right
+
+#### Plot1
+colors=['lightgray', 'lightpink', 'royalblue', 'red']
+sns.barplot(x="Gender_Predicted", y="avg_accuracy", hue="Gender_Predicted", data=dt, order=[1,2,3,0], dodge=False, ax=ax1, palette=colors)
+ax1.set_ylabel('Average Accuracy', fontsize=7)
+ax1.set_xlabel('Genders', fontsize=7)
+ax1.set_xticklabels(['Female', 'Male', 'Both', 'No Fish'])
+ax1.set_title('General Analysis', fontsize=10)
+ax1.get_legend().set_visible(False)
+
+
+#### Plot 2
+x2_labels = dt_f.avg_accuracy.round(2)
+sns.barplot(x="avg_accuracy", y="Framefile", data=dt_f, color='lightpink', ax=ax2)
+ax2.set_xlabel('Average Accuracy', fontsize=7)
+ax2.set_xticklabels(x2_labels)
+ax2.set_ylabel('No. of Framefiles', fontsize=7)
+ax2.set_title('Only Females Present', fontsize=10)
+
+
+#### Plot 3
+x3_labels = dt_m.avg_accuracy.round(2)
+sns.barplot(x="avg_accuracy", y="Framefile", data=dt_m, color='royalblue', ax=ax3)
+ax3.set_xlabel('Average Accuracy', fontsize=7)
+ax3.set_xticklabels(x3_labels)
+ax3.set_ylabel('No. of Framefiles', fontsize=7)
+ax3.set_title('Only Males Present', fontsize=10)
+
+
+#### Plot 4
+x4_labels = dt_b.avg_accuracy.round(2)
+sns.barplot(x="avg_accuracy", y="Framefile", data=dt_b, color='orange', ax=ax4)
+ax4.set_xlabel('Average Accuracy', fontsize=7)
+ax4.set_xticklabels(x4_labels)
+ax4.set_ylabel('No. of Framefiles', fontsize=7)
+ax4.set_title('Both Males & Females Present', fontsize=10)
+
+plt.show()
+
