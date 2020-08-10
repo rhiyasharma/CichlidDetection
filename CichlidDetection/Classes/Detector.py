c29863731cd01df758fd20cf2737878a64aaaee1
rsharma348 <rsharma348@gatech.edu>
2020-08-10 14:14:46 -0400

    print statements

--- CichlidDetection/Classes/Detector.py
+++ CichlidDetection/Classes/Detector.py
@@ -62,9 +62,11 @@ class Detector:
             path (str): path to the video directory (see ProjectFileManager)
         """
         video_name = path.split('/')[-1].split('.')[0]
+        print('beginning loading')
         dataset = DetectVideoDataSet(Compose([ToTensor()]), path, self.pfm)
         dataloader = DataLoader(dataset, batch_size=5, shuffle=False, num_workers=8, pin_memory=True,
                                 collate_fn=collate_fn)
+        print('done loading')
         self.evaluate(dataloader, "{}_{}".format(pid, video_name))
 
     def _initiate_model(self):
