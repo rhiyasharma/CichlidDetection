d5b7d1a155318d769fcb0726301c7ea11b3fae56
rsharma348 <rsharma348@gatech.edu>
2020-08-21 17:43:48 -0400

    Removed animation function -- using cv2 instead

--- CichlidDetection/Classes/VideoCreator.py
+++ CichlidDetection/Classes/VideoCreator.py
@@ -1,80 +1,72 @@
+import sys
 import os
-import matplotlib.image as mpimg
-from PIL import Image
-import matplotlib.pyplot as plt
+import cv2
 import pandas as pd
-import numpy as np
-from matplotlib.animation import FuncAnimation
-from matplotlib.patches import Rectangle
-
+from os.path import join
 from CichlidDetection.Classes.FileManager import FileManager
-
-pd.set_option('display.expand_frame_repr', False)
+from CichlidDetection.Classes.FileManager import ProjectFileManager
 
 
 def convert_pos(x1, y1, x2, y2):
-    return [x1, y1, x2 - x1, y2 - y1]
+    x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+    return [(x1, y1), (x2, y2)]
 
+class VideoAnnotation:
 
-class Animation:
-
-    def __init__(self, pid, video_name, csv_file, *args):
+    def __init__(self, pid, video, csv_file, *args):
 
         self.fm = FileManager()
         for i in args:
             self.pfm = i
-        # collect a good set of frames to animate
         self.detection_dir = self.fm.local_files['detection_dir']
-        self.video_name = video_name.split('.')[0] + '.npy'
-        self.animation_name = video_name.split('.')[0]
-        self.frames = np.load(os.path.join(self.pfm.local_files['{}_dir'.format(pid)], self.video_name))
+        self.video = video
+        self.video_name = video.split('.')[0]
+        self.ann_video_name = 'annotated_' + video.split('.')[0]
         self.csv_file_path = os.path.join(self.detection_dir, csv_file)
 
-    def animated_learning(self):
-        """for a single frame, successively plot the predicted boxes and labels at each epoch to create an animation"""
-
-        df = pd.read_csv(os.path.join(self.csv_file_path))
-        df = df[['Framefile', 'boxes', 'labels', 'scores']]
+    def annotate(self):
+        """for a each frame, successively plot the predicted boxes and labels to create a video"""
+        df = pd.read_csv(self.csv_file_path)
         df[['boxes', 'labels']] = df[['boxes', 'labels']].applymap(lambda x: eval(x))
         df['order'] = df.apply(lambda x: int(x.Framefile.split('.')[0].split('_')[1]), axis=1)
-        # df = df.sort_values(by=['order']).reset_index()
-        # df['len'] = df['boxes'].apply(lambda x: len(x))
 
-        # build up the animation
-        fig = plt.figure()
-        max_detections = 5
-        ax = fig.add_subplot(111)
+        cap = cv2.VideoCapture(self.video)
+        vid_len = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        frame_width = int(cap.get(3))
+        frame_height = int(cap.get(4))
+        size = (frame_width, frame_height)
 
-        boxes = [Rectangle((0, 0), 0, 0, fill=False) for _ in range(max_detections)]
+        result = cv2.VideoWriter(self.ann_video_name, cv2.VideoWriter_fourcc(*"mp4v"), 5, size)
 
-        def animate(i):
-            new_frame = df.iloc[i].Framefile
-            new_img = Image.fromarray(self.frames[i], 'RGB')
-            # new_img = mpimg.imread(os.path.join(self.frame_dir, new_frame))
-            img = plt.imshow(new_img)
-            ax = plt.gca()
-            label_preds = df.labels[i]
-            label_preds = (label_preds + ([0] * max_detections))[:5]
-            box_preds = df.boxes[i]
-            box_preds = [convert_pos(*p) for p in box_preds]
-            box_preds = (box_preds + ([[0, 0, 0, 0]] * max_detections))[:5]
-            color_lookup = {0: 'None', 1: '#FF1493', 2: '#00BFFF'}
-            for j in range(5):
-                boxes[j].set_xy([box_preds[j][0], box_preds[j][1]])
-                boxes[j].set_width(box_preds[j][2])
-                boxes[j].set_height(box_preds[j][3])
-                boxes[j].set_edgecolor(color_lookup[label_preds[j]])
+        count = 0
+        for i in range(vid_len):
+            ret, frame = cap.read()
+            if not ret:
+                print("VideoError: Couldn't read frame " + str(i) in self.video_name + ". ", file=sys.stderr)
+                break
+            else:
+                label_preds = df.labels[i]
+                box_preds = df.boxes[i]
+                box_preds = [convert_pos(*p) for p in box_preds]
+                if len(label_preds) > 0:
+                    for j in range(len(label_preds)):
+                        start, end = box_preds[0][0], box_preds[0][1]
+                        color_lookup = {1: (255, 153, 255), 2: (255, 0, 0)}
+                        cv2.rectangle(frame, (start[0], start[1]), (end[0], end[1]), color_lookup[label_preds[j]], 2)
+                        result.write(frame)
+                        print('Completed Annotated Frame {}'.format(count))
+                        if cv2.waitKey(1) & 0xFF == ord('q'):
+                            break
+                else:
+                    result.write(frame)
+                    print('Completed Frame {}'.format(count))
+                    if cv2.waitKey(1) & 0xFF == ord('q'):
+                        break
 
-            for box in boxes:
-                ax.add_patch(box)
+            count += 1
 
-            img.set_array(new_img)
+        cap.release()
+        result.release()
+        cv2.destroyAllWindows()
 
-            print('Completed ', new_frame)
-
-            return [img]
-
-        anim = FuncAnimation(fig, animate, frames=len(df)-1, blit=True, interval=200, repeat=False)
-        anim.save(os.path.join(self.detection_dir, '{}_detections.gif'.format(self.animation_name)), writer='imagemagick')
-        plt.close('all')
-
+        print("The detection video was successfully saved")
