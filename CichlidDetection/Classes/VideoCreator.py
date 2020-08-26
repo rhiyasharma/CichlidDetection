4fa7cd0eceaaea93ae9deb3550530e4cf85bc49b
rsharma348 <rsharma348@gatech.edu>
2020-08-26 13:17:24 -0400

    checking location

--- CichlidDetection/Classes/VideoCreator.py
+++ CichlidDetection/Classes/VideoCreator.py
@@ -56,12 +56,12 @@ class VideoAnnotation:
                         color_lookup = {1: (255, 153, 255), 2: (255, 0, 0)}
                         cv2.rectangle(frame, (start[0], start[1]), (end[0], end[1]), color_lookup[label_preds[j]], 2)
                         result.write(frame)
-                        print('Completed Annotating Frame {}'.format(count))
+                        # print('Completed Annotating Frame {}'.format(count))
                         if cv2.waitKey(1) & 0xFF == ord('q'):
                             break
                 else:
                     result.write(frame)
-                    print('Completed Frame {}'.format(count))
+                    # print('Completed Frame {}'.format(count))
                     if cv2.waitKey(1) & 0xFF == ord('q'):
                         break
 
